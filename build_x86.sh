37190fd23ba6d66740a61a3d8aac08a89d1ae499
Rakesh Ranjan <rakesh.safir@gmail.com>
2017-01-24 00:46:45 +0530

    Added submodule and half baked script to build everything

---
+++ build_x86.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+PACKAGE_LIST=(libxml2-dev
+)
+
+function install_packages() {
+	
+	for(( i=0; i<${#PACKAGE_LIST[@]} ; ++i )) ; do
+		echo -n "Checking ["$((i+1))"/${#PACKAGE_LIST[@]}]:{${PACKAGE_LIST[$i]}} => "
+		if [[ -z $(dpkg -l | grep ${PACKAGE_LIST[$i]} | awk '{print $2, $3}') ]]; then
+			echo -n "Installing [${PACKAGE_LIST[$i]}]..."
+			apt-get install -y ${PACKAGE_LIST[$i]} 2>&1 > /dev/null
+			if [[ $? -eq 0 ]]; then
+				echo "Success..."
+			else
+				echo "Failed..."
+			fi
+		else
+			echo "Installed..."
+		fi
+	done
+}
+
+
+function build_envytools() {
+	cd envytools
+	mkdir build
+	cd build
+	cmake -DCMAKE_INSTALL_PREFIX=$(readlink -f ../../build) ..
+	make && make install
+	cd ../../
+}
+
+function build_libdrm_pscnv() {
+	cd libdrm_pscnv
+	./autogen.sh --prefix=$(readlink -f ../build) --enable-nouveau-experimental-api
+	make && make install
+	cd ..
+}
+
+function build_pscnv() {
+	export PATH=$(readlink -f build/bin):$PATH
+	cd pscnv
+	mkdir build
+	cd build
+	cmake -DCMAKE_INSTALL_PREFIX=$(readlink -f ../../build) -DCMAKE_SYSROOT=$(readlink -f ../../build) -DCMAKE_CXX_FLAGS=-I$(readlink -f ../../build/include/libdrm) ..
+	make && make install
+	cd ../../
+}
+
+
+function main() {
+	install_packages
+	build_envytools
+}
+
+main
+	
